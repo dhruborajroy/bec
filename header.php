fcd087e64389f676540e8e49e351fdb404bb0d8c
Dhrubo <dhruborajroy3@gmail.com>
2025-01-05 18:24:57 +0600

    index page

---
+++ header.php
@@ -0,0 +1,159 @@
+<!DOCTYPE html>
+<html lang="en">
+  
+<head>
+    <meta charset="utf-8">
+    <meta name="keywords" content="HTML5 Template" />
+    <meta name="description" content="Academic - Education, University and College Template" />
+    <meta name="author" content="potenzaglobalsolutions.com" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Academic - Education, University and College Template</title>
+
+    <!-- Favicon -->
+    <link rel="shortcut icon" href="images/favicon.ico" />
+    <!-- Google Font -->
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,200&amp;display=swap" rel="stylesheet">
+
+    <!-- CSS Global Compulsory (Do not remove)-->
+    <link rel="stylesheet" href="css/font-awesome/all.min.css" />
+    <link rel="stylesheet" href="css/flaticon/flaticon.css" />
+    <link rel="stylesheet" href="css/bootstrap/bootstrap.min.css" />
+
+    <!-- Page CSS Implementing Plugins (Remove the plugin CSS here if site does not use that feature)-->
+    <link rel="stylesheet" href="css/select2/select2.css" />
+    <link rel="stylesheet" href="css/owl-carousel/owl.carousel.min.css" />
+    <link rel="stylesheet" href="css/magnific-popup/magnific-popup.css" />
+    <link rel="stylesheet" href="css/swiper/swiper.min.css" />
+    <link rel="stylesheet" href="css/animate/animate.min.css"/>
+
+    <!-- Template Style -->
+    <link rel="stylesheet" href="css/style.css" />
+  </head>
+
+  <body>
+
+    <!--=================================
+    Header -->
+    <header class="header header-sticky">
+      <div class="topbar bg-dark py-3 d-none d-lg-flex">
+        <div class="container">
+          <div class="row">
+            <div class="col-sm-12">
+              <div class="d-none d-lg-flex align-items-center text-center">
+                <div class="mr-3 d-inline-block">
+                  <a class="text-white" href="mailto:gethelp@university.com"><i class="far fa-envelope mr-2"></i><span>Email:</span> info@academic.com</a>
+                </div>
+                <div class="mr-auto d-inline-block">
+                  <a class="text-white" href="mailto:gethelp@university.com"><i class="fas fa-phone-alt mr-2"></i><span>Call</span> +1 (818) 89 89 567</a>
+                </div>
+                <div class="mr-3 d-inline-block">
+                  <a class="text-white" href="mailto:gethelp@university.com"><i class="far fa-clock mr-2"></i><span>Opening:</span> 10:00am - 5:00pm</a>
+                </div>
+                <div class="social-icon">
+                  <ul class="social-icon">
+                    <li>
+                      <a href="#"><i class="fab fa-facebook-f"></i></a>
+                    </li>
+                    <li>
+                      <a href="#"><i class="fab fa-twitter"></i></a>
+                    </li>
+                    <li>
+                      <a href="#"><i class="fab fa-linkedin-in"></i></a>
+                    </li>
+                    <li>
+                      <a href="#"><i class="fab fa-instagram"></i></a>
+                    </li>
+                  </ul>
+                </div>
+
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="header-main py-3">
+        <div class="container">
+          <div class="row">
+            <div class="col-sm-12">
+              <div class="d-lg-flex align-items-center">
+                <!-- logo -->
+                <a class="navbar-brand logo" href="index.php">
+                  <img src="images/logo.svg" alt="Logo">
+                </a>
+                <nav class="navbar navbar-expand-lg">
+
+                <!-- Navbar toggler START-->
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                  <span class="navbar-toggler-icon"></span>
+                </button>
+                <!-- Navbar toggler END-->
+
+                <!-- Navbar START -->
+                <div class="collapse navbar-collapse justify-content-center" id="navbarSupportedContent">
+                  <ul class="navbar-nav">
+                    <li class="nav-item dropdown active">
+                      <a class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Home</a>
+                    </li>
+                    <li class="nav-item dropdown">
+                      <a class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Pages<i class="fas fa-chevron-down fa-xs"></i>
+                      </a>
+                      <!-- Dropdown Menu -->
+                      <ul class="dropdown-menu">
+                        <li><a class="dropdown-item" href="about-us.php">About Us</a></li>
+                        <li><a class="dropdown-item" href="teachers.php">Teachers</a></li>
+                        <li><a class="dropdown-item" href="teachers-single.php">Teacher Single</a></li>
+                        <li><a class="dropdown-item" href="gallery.php">Gallery</a></li>
+                        <li><a class="dropdown-item" href="pricing.php">Pricing</a></li>
+                        <li><a class="dropdown-item" href="faq.php">Faq</a></li>
+                        <li><a class="dropdown-item" href="sign-in.php">Sign In</a></li>
+                        <li><a class="dropdown-item" href="sign-up.php">Sign Up</a></li>
+                        <li><a class="dropdown-item" href="error-page.php">Error Page</a></li>
+                        <li><a class="dropdown-item" href="coming-soon.php">Coming Soon</a></li>
+                        <li><a class="dropdown-item" href="privacy-policy.php">Privacy Policy</a></li>
+                        <li><a class="dropdown-item" href="terms-and-conditions.php">Terms & Conditions</a></li>
+                      </ul>
+                    </li>
+                    <li class="nav-item dropdown">
+                      <a class="nav-link  dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Course<i class="fas fa-chevron-down fa-xs"></i>
+                      </a>
+                      <!-- Dropdown Menu -->
+                      <ul class="dropdown-menu">
+                          <li><a class="dropdown-item" href="course.php">Course</a></li>
+                          <li><a class="dropdown-item" href="course-detail.php">Course Detail</a></li>
+                      </ul>
+                    </li>
+                    <li class="nav-item dropdown">
+                      <a class="nav-link  dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Event<i class="fas fa-chevron-down fa-xs"></i>
+                      </a>
+                      <!-- Dropdown Menu -->
+                      <ul class="dropdown-menu">
+                          <li><a class="dropdown-item" href="events-list.php">Events List</a></li>
+                          <li><a class="dropdown-item" href="events-grid.php">Events Grid</a></li>
+                          <li><a class="dropdown-item" href="event-detail.php">Event Detail</a></li>
+                      </ul>
+                    </li>
+                    <li class="nav-item dropdown">
+                      <a class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Blog<i class="fas fa-chevron-down fa-xs"></i>
+                      </a>
+                      <!-- Dropdown Menu -->
+                      <ul class="dropdown-menu">
+                          <li><a class="dropdown-item" href="blog.php">Blog</a></li>
+                          <li><a class="dropdown-item" href="blog-detail.php">Blog Detail</a></li>
+                      </ul>
+                    </li>
+                    <li class="nav-item">
+                      <a class="nav-link" href="contact-us.php">Contact Us</a>
+                    </li>
+                  </ul>
+                </div>
+                <!-- Navbar END-->
+                </div>
+              </nav>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </header>
+    <!--=================================
+    Header -->
